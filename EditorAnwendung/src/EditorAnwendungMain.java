b048b46c97e5564643265b9bc72ab1f66405711b
binderli <binder.li@outlook.de>
2018-11-13 09:01:59 +0100

    Added a comment for test

--- EditorAnwendung/src/EditorAnwendungMain.java
+++ EditorAnwendung/src/EditorAnwendungMain.java
@@ -2,9 +2,9 @@
 public class EditorAnwendungMain {
 
 	public static void main(String[] args) {
-		EditorAnwendung gui = new EditorAnwendung();
+		final EditorAnwendung gui = new EditorAnwendung();
 		gui.open();
-		
+		// Test kommentar für Git
 	}
 
 }
